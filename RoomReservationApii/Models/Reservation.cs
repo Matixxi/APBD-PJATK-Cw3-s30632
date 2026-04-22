6ebe54e9ef72c7edc87f68259aa693db16966c9d
Mateusz Siemek <mateusz@siemek.pl>
2026-04-22 23:38:24 +0200

    Add Room and Reservation models

---
+++ RoomReservationApii/Models/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomReservationApii.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        public int RoomId { get; set; }
+
+        [Required]
+        public string OrganizerName { get; set; } = string.Empty;
+
+        [Required]
+        public string Topic { get; set; } = string.Empty;
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
+        public string Status { get; set; } = "planned";
+    }
+}
